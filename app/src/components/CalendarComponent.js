5e4d68172f813923945bc8b6eeb1da847f0e0179
sharonrub <sharon.rub88@gmail.com>
2018-09-05 15:49:00 +0300

    calendar working

--- app/src/components/CalendarComponent.js
+++ app/src/components/CalendarComponent.js
@@ -25,46 +25,70 @@ class CalendarComponent extends Component {
         }
     }
 
-    componentDidMount () {
-        console.log("Inside CalendarComponent componentDidMount");
-    }
 
-    shouldComponentUpdate(nextProps, nextState) {
+    componentWillReceiveProps(nextProps) {
         console.log('Inside CalendarComponent shouldComponentUpdate');
-        console.log(this.props);
-        console.log(nextProps);
+        // console.log(this.props);
+        // console.log(nextProps);
         if (JSON.stringify(this.props) === JSON.stringify(nextProps)){
             return false;
         }
+        this.props = nextProps;
+        let day, time, event, updatedEvents;
+        let determineDay = function (day) {
+            if (day === "Sunday") return '2018-04-01';
+            else if (day === "Monday") return '2018-04-02';
+            else if (day === "Tuesday") return '2018-04-03';
+            else if (day === "Wednesday") return '2018-04-04';
+            else if (day === "Thursday") return '2018-04-05';
+            else if (day === "Friday") return '2018-04-06';
+        };
+        console.log(this.props);
+        day = determineDay(this.props.courseObject[0].day);
+        time = this.props.courseObject[0].hours.split(['-']);
+        event = {
+            title: `Course Number: ${this.props.courseObject[0].courseGroupId.courseNum},
+                Location: ${this.props.courseObject[0].courseGroupId.groupLocation}`,
+            start: `${day}T${time[0]}`,
+            end: `${day}T${time[1]}`
+        };
+        updatedEvents = [...this.state.events];
+        updatedEvents.push(event);
+        console.log(updatedEvents);
+        this.setState ({events: updatedEvents});
+        console.log(this.state);
         return true;
     }
 
-    componentDidUpdate(){
-        console.log("Inside CalendarComponent componentDidUpdate");
-        let day, time, event, updatedEvents;
-            let determineDay = function (day) {
-                if (day === "Sunday") return '2018-04-01';
-                else if (day === "Monday") return '2018-04-02';
-                else if (day === "Tuesday") return '2018-04-03';
-                else if (day === "Wednesday") return '2018-04-04';
-                else if (day === "Thursday") return '2018-04-05';
-                else if (day === "Friday") return '2018-04-06';
-            };
-            day = determineDay(this.props.courseGroupObject[0].day);
-            time = this.props.courseGroupObject[0].hours.split(['-']);
-            event = {
-                title: `Course Number: ${this.props.courseGroupObject[0].courseGroupId.courseNum},
-                Location: ${this.props.courseGroupObject[0].courseGroupId.groupLocation}`,
-                start: `${day}T${time[0]}`,
-                end: `${day}T${time[1]}`
-            };
-            updatedEvents = [...this.state.events];
-            updatedEvents.push(event);
-            console.log(updatedEvents);
-            this.setState ({events: updatedEvents})
-    }
+    // componentDidUpdate(){
+    //     console.log("Inside CalendarComponent componentDidUpdate");
+    //     let day, time, event, updatedEvents;
+    //         let determineDay = function (day) {
+    //             if (day === "Sunday") return '2018-04-01';
+    //             else if (day === "Monday") return '2018-04-02';
+    //             else if (day === "Tuesday") return '2018-04-03';
+    //             else if (day === "Wednesday") return '2018-04-04';
+    //             else if (day === "Thursday") return '2018-04-05';
+    //             else if (day === "Friday") return '2018-04-06';
+    //         };
+    //         console.log(this.props);
+    //         day = determineDay(this.props.courseGroupObject[0].day);
+    //         time = this.props.courseGroupObject[0].hours.split(['-']);
+    //         event = {
+    //             title: `Course Number: ${this.props.courseGroupObject[0].courseGroupId.courseNum},
+    //             Location: ${this.props.courseGroupObject[0].courseGroupId.groupLocation}`,
+    //             start: `${day}T${time[0]}`,
+    //             end: `${day}T${time[1]}`
+    //         };
+    //         updatedEvents = [...this.state.events];
+    //         updatedEvents.push(event);
+    //         console.log(updatedEvents);
+    //         this.setState ({events: updatedEvents})
+    // }
 
     render () {
+        console.log("inside CalendarComponent render");
+        console.log(this.state);
         let {events, options} = this.state;
         return (
             <FullCalendar
@@ -74,7 +98,6 @@ class CalendarComponent extends Component {
         )
     }
 }
-
 
 export default CalendarComponent;
 
